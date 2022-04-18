88751da7b2d3831c88f6ac475d5e5fe5a339b17b
scottharrisongit <45775539+scottharrisongit@users.noreply.github.com>
2022-04-18 11:56:40 -0400

    Update install.R
    Doing more to have needed R packages be initially installed, while still checking and addressing them if they cannot be loaded.

--- install.R
+++ install.R
@@ -1,3 +1,25 @@
-install.packages("markdown",ask=FALSE)
-install.packages("rmarkdown",ask=FALSE)
-install.packages("tinytex",ask=FALSE)
+# A function to avoid installing packages if already present.
+install_packages <- function (package_names)
+{
+  for (package_name in package_names) {
+    if (!require(package_name,character.only=TRUE)) {
+      do.call("install.packages",list(package_name,ask=FALSE))
+    }
+    library(package_name,character.only=TRUE)
+  }
+}
+
+# These packages are needed for R markdown documents within the R Studio environment.
+install_packages(c("markdown","rmarkdown","tinytex"))
+
+# These packages are needed for directly support the R commands.
+install.packages("rlang") # ensure this is the most up-to-date version for dplyr
+library(rlang)
+install_packages(c("rlang","BiocManager","ggplot2","ggrepel","rlang","dplyr"))
+
+# Install and load the following Bioconductor-based packages.
+BiocManager::install(c("limma","pheatmap","ExpressionAtlas"),ask=FALSE)
+
+library(limma)
+library(pheatmap)
+library(ExpressionAtlas)
